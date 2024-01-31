d75be2580d20595c25488e8d169b598c4d701fcf
Mateusz <mateuszmaslanka06@gmail.com>
2024-01-31 12:59:41 +0100

    modifying additional vinfo

--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -3,10 +3,19 @@ import "./Notes.scss";
 import VehicleInfo from '../VehicleInfo/VehicleInfo';
 
 function Notes(){
+
+    const [vehicleInfoVisible, setVehicleInfoVisible] = React.useState();
+
+        
+    function changeVehicleInfo(){
+        let visibility = vehicleInfoVisible;
+        setVehicleInfoVisible(!visibility);
+    }
+
     return (
-        <div className='notesContainer'>
-            <div className="vehicleInfo pt-1">
-                <VehicleInfo />
+        <div className='notesContainer' onClick={changeVehicleInfo}>
+            <div className="vehicleInfo pt-1" >
+                {vehicleInfoVisible && <VehicleInfo/>}
             </div>
         </div>
     )
