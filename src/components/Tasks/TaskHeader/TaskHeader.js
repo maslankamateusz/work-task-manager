6075af69d827bbbfaee6382b410975188de6e663
Mateusz <mateuszmaslanka06@gmail.com>
2024-02-04 22:57:50 +0100

    TaskHeader rebuilding

--- src/components/Tasks/TaskHeader/TaskHeader.js
+++ src/components/Tasks/TaskHeader/TaskHeader.js
@@ -1,11 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./TaskHeader.scss";
 
 function TaskHeader(props) {
   const [newTasks, setNewTasks] = useState({
     newTaskName: "",
     newTaskDate: getTodayDate(),
+    newTaskStatus: "", 
   });
+
+  const taskColors = [
+    { status: "to-do", color: "warning" },
+    { status: "planed", color: "secondary" },
+    { status: "in-progress", color: "primary" },
+    { status: "done", color: "success" }
+  ];
+
+  const [defaultValuesSet, setDefaultValuesSet] = useState(false);
+
+  useEffect(() => {
+    if (!defaultValuesSet) {
+      setNewTasks((prev) => ({ ...prev, newTaskStatus: "to-do" }));
+      setDefaultValuesSet(true);
+    }
+  }, [defaultValuesSet]);
+
+  function getTaskColor(taskStatus) {
+    const matchingColor = taskColors.find((item) => item.status === taskStatus);
+    return matchingColor ? matchingColor.color : "secondary";
+  }
 
   function updateNewTask(event) {
     setNewTasks((prev) => ({ ...prev, newTaskName: event.target.value }));
@@ -13,12 +35,15 @@ function TaskHeader(props) {
 
   function saveNewTask() {
     const formattedDate = formatDate(newTasks.newTaskDate);
+
     props.createNewTaskCallback({
       newTaskName: newTasks.newTaskName,
       date: formattedDate,
-      color: "secondary",
+      color: getTaskColor(newTasks.newTaskStatus),
+      status: newTasks.newTaskStatus,
     });
-    setNewTasks((prev) => ({ ...prev, newTaskName: "", newTaskDate: getTodayDate() }));
+
+    setNewTasks((prev) => ({ ...prev, newTaskName: "" }));
   }
 
   function getTodayDate() {
@@ -41,7 +66,7 @@ function TaskHeader(props) {
           <span className="input-group-text bg-light">Task:</span>
           <input
             type="text"
-            className='form-control'
+            className='form-control w-auto'
             id='task-name'
             placeholder='Task name'
             value={newTasks.newTaskName}
@@ -49,10 +74,19 @@ function TaskHeader(props) {
           />
           <input
             type="date"
-            className='form-control form-control-sm  '
+            className='form-control form-control-sm ps-2'
             id="task-date"
             onChange={(e) => setNewTasks((prev) => ({ ...prev, newTaskDate: e.target.value }))}
           />
+          <select
+            id="taskStatus"
+            onChange={(e) => setNewTasks((prev) => ({ ...prev, newTaskStatus: e.target.value }))}
+            className='form-select ps-3 w-25 select-status'>
+            <option value="to-do">To do</option>
+            <option value="planed">Planed</option>
+            <option value="in-progress">In progress</option>
+            <option value="done">Done</option>
+          </select>
           <button
             className='btn btn-success btn-sm'
             type='button'
--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -8,7 +8,7 @@ function Tasks() {
       { id: 0, name: "Task 1", date: "30.01", status: "ToDo", color: "primary" },
       { id: 1, name: "Task 2", date: "03.02", status: "Done", color: "success" }
     ]);
-  
+    
     function getTaskItems() {
       return tasks.map(item => <TaskItem key={item.id} {...item} deleteNewTaskCallback={deleteTask} />);
     }
@@ -17,7 +17,7 @@ function Tasks() {
       const newId = tasks.length;
       setTasks(prevTasks => [
         ...prevTasks,
-        { id: newId, name: task.newTaskName, date: task.date, status: "in progress", color: task.color }
+        { id: newId, name: task.newTaskName, date: task.date, status: task.status, color: task.color }
       ]);
     }
   
