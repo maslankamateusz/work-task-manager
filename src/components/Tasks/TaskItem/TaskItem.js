6c47e657893f785381ca3d839332ffe204dde7fb
Mateusz <mateuszmaslanka06@gmail.com>
2024-02-07 21:48:48 +0100

    edit task option v1

--- src/components/Tasks/TaskItem/TaskItem.js
+++ src/components/Tasks/TaskItem/TaskItem.js
@@ -1,47 +1,57 @@
 import React from 'react';
 import "./TaskItem.scss";
+import TaskItemEdit from "./TaskItemEdit/TaskItemEdit";
 
 function TaskItem(props){
+
+    const [isEditing, setIsEditing] = React.useState(false);
 
     function deleteTask(){
         let taskId = props.id;
         props.deleteNewTaskCallback(taskId);
     }
     function editTask(){
-        console.log("edit");
+        setIsEditing(!isEditing);
     }
     function getTaskNumber(){
         return `Task: ${props.id+1}`;
     }
     getTaskNumber();
-    return (
-        <div className='d-flex pt-2 border-bottom justify-content-between' id={props.id}>
-            <div className='w-20'>
-                <p className='flex-grown-1 mb-0 pb-3 mx-2 small lh-sm text-start'>
-                    <strong className='d-block my-1'>
-                        <span className={"badge bg-"+ props.color}>
-                            {props.status}
-                        </span>
-                        <span className="mx-2 ">
-                            {props.date}
-                        </span>
-                    </strong>
-                    {getTaskNumber()}
-                </p>
+    if(isEditing){
+        return(
+            <TaskItemEdit />
+        )
+    } else{
+        
+        return (
+            <div className='d-flex pt-2 border-bottom justify-content-between' id={props.id}>
+                <div className='w-20'>
+                    <p className='flex-grown-1 mb-0 pb-3 mx-2 small lh-sm text-start'>
+                        <strong className='d-block my-1'>
+                            <span className={"badge bg-"+ props.color}>
+                                {props.status}
+                            </span>
+                            <span className="mx-2 ">
+                                {props.date}
+                            </span>
+                        </strong>
+                        {getTaskNumber()}
+                    </p>
+                </div>
+                <div className='mt-0 w-50 me-5 text-start d-flex align-items-center'>
+                    <p className='fs-3 '>{props.name}</p>
+                </div>
+                <div className='me-3 mt-2 ' >
+                    <button className='btn btn-sm btn-outline-success me-2' onClick={editTask}>
+                        Edit
+                    </button>
+                    <button className='btn btn-sm btn-outline-danger ' onClick={deleteTask}>
+                        Close
+                    </button>
+                </div>
             </div>
-            <div className='mt-0 w-50 me-5 text-start d-flex align-items-center'>
-                <p className='fs-3 '>{props.name}</p>
-            </div>
-            <div className='me-3 mt-2 ' >
-                <button className='btn btn-sm btn-outline-success me-2' onClick={editTask}>
-                    Edit
-                </button>
-                <button className='btn btn-sm btn-outline-danger ' onClick={deleteTask}>
-                    Close
-                </button>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default TaskItem;
---
+++ src/components/Tasks/TaskItem/TaskItemEdit/TaskItemEdit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./TaskItemEdit.scss";
+
+function TaskItemEdit(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
+export default TaskItemEdit;
