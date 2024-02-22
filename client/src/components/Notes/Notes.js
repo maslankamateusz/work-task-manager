d776e6c6fe96f4422fde16b554f807036339592e
Mateusz <mateuszmaslanka06@gmail.com>
2024-02-22 19:53:34 +0100

    small fix

--- client/src/components/Notes/Notes.js
+++ client/src/components/Notes/Notes.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import "./Notes.scss";
-import VehicleInfo from '../VehicleInfo/VehicleInfo';
 
 function Notes(){
 
-    const [vehicleInfoVisible, setVehicleInfoVisible] = React.useState();
-
-        
-    function changeVehicleInfo(){
-        let visibility = vehicleInfoVisible;
-        setVehicleInfoVisible(!visibility);
-    }
 
     return (
-        <div className='notesContainer' onClick={changeVehicleInfo}>
-            <div className="vehicleInfo pt-1" >
-                {vehicleInfoVisible && <VehicleInfo/>}
-            </div>
+        <div className='notesContainer mb-4 mb-lg-0' >
+            notes
         </div>
     )
 }
